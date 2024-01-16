063a8df0f3e2f50aeb54e5840af394e31ae9d5e3
Khushi Kesharwani <khushibharatkesharwani@gmail.com>
2024-01-16 21:30:21 +0530

    Initial commit

---
+++ src/LinkWithRef.jsx
@@ -0,0 +1,11 @@
+// LinkWithRef.js
+import React, { useContext } from 'react';
+import { YourContext } from './YourContext';
+
+const LinkWithRef = () => {
+  const { basename } = useContext(YourContext);
+
+  // Rest of the component logic
+};
+
+export default LinkWithRef;
